d92ad85236f4a0968cda810ad24dec921242adec
Hugo Cremers <hcremers@student.s19.be>
2023-02-05 18:40:04 +0100

    Inception v0.6 Project started back from scratch

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -6,31 +6,41 @@
 #    By: hcremers <hcremers@student.s19.be>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/18 13:44:00 by hcremers          #+#    #+#              #
-#    Updated: 2023/01/25 15:03:16 by hcremers         ###   ########.fr        #
+#    Updated: 2023/02/05 18:21:00 by hcremers         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-# Check if the setup file exists
-cat .setup 2> /dev/null
+# Change the owner of the files
+chown -R mysql:mysql /var/lib/mysql
 
-if [$? -ne 0];
-	then
+# Check if the file already exists
+if [ ! -d /var/lib/mysql/$MARIADB_DB ]; then
 
-	# Define the directory and execute mysqld_safe
-	usr/bin/mysqld_safe --datadir=/var/lib/mysqld &
+	# Start mysql in the right directory
+	service mysql start --datadir=/var/lib/mysql
 
-	# Wait until the mysql server is up
-	while ! mysqladmin ping -h "$MARIADB_HOST" --silent;
-		do
-		sleep 1
-	done
+	# Create directory and file for the daemon
+	mkdir -p /var/run/mysqld
+	touch /var/run/mysqld/mysqlf.pid
 
-	# Execute the sql script in mariadb
-	eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
+	# Execute the SQL script in MariaDB
+	eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb -u root
 
-	# Create the setup file
-	touch .setup
+	# Set MySQL root password
+	mysqladmin -u root password $MARIADB_ROOT_PWD
+
+	service mysql stop --datadir=/var/lib/mysql
+
+else
+	# Create directory and file for the daemon
+	mkdir -p /var/run/mysqld
+	touch /var/run/mysqld/mysqlf.pid
+	# mkfifo /var/run/mk
+
 fi
 
-# Execute mysqld_safe and define the directory
-/usr/bin/mysqld_safe --datadir=/var/lib/mysql
+# Change the owner of those files
+chown -R mysql:mysql /var/run/mysqld
+
+# Start the daemon
+mysqld_safe --datadir=/var/lib/mysql
